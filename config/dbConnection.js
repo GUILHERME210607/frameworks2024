447d0209aa0b9d0ed0c21f267885297e0f9067af
GUILHERME210607 <junaramariane7@gmail.com>
2024-03-15 18:30:43 -0300

    teste

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 module.exports = function (){
     return mysql.createConnection({
         host: 'localhost',
-        user: 'ifms',
+        user: 'root',
         password: 'ifms',
         database: 'portal_noticias'
     });
