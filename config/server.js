c9dc8bced3abbd05c9e471bc46507b068c1c246f
Guilherme <guilherme.souza12@estudante.ifms.edu.br>
2024-06-11 16:03:51 -0300

    arrumando meu codigo

--- config/server.js
+++ config/server.js
@@ -7,5 +7,8 @@ app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engr
 app.set('views','./app/views'); // indica o caminho das viwes
 app.use(express.static("./app/public/"));
 consign().include("app/routes").into(app);
+app.use(function(req,res){
+    res.status(404).render("./secao/404")
+});
 module.exports = app;
 
