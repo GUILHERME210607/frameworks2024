e7c16589a8046078dcdf6f937700c9ef81a41dfd
Guilherme <guilherme.souza12@estudante.ifms.edu.br>
2024-06-13 17:40:03 -0300

    save

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,10 @@
-var dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
-
-    var connection = dbConnection();
-    app.get('/noticias', function (req, res) {
-
-        connection.query('select * from noticias', function (error, result) {
-            res.render("noticias/noticias", { noticias: result });
-        });
+    app.get("/noticias", function (req, res) {
+        var conexao = app.config.dbConnection();
+        conexao.query('select * from noticias', function (error, result) {
+                res.render("noticias/noticias", { noticias: result });
+            });
 
     })
 }
--- config/server.js
+++ config/server.js
@@ -6,9 +6,14 @@ var app = express();
 app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engrenagem das views
 app.set('views','./app/views'); // indica o caminho das viwes
 app.use(express.static("./app/public/"));
-consign().include("app/routes").into(app);
+consign()
+.include("app/routes")
+.then("config/dbConnection.js")
+.into(app);
+
 app.use(function(req,res){
     res.status(404).render("./secao/404")
 });
+
 module.exports = app;
 
