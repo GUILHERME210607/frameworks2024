928308016cbbad828fdcc16b205ab1e7bcd873a8
Guilherme <guilherme.souza12@estudante.ifms.edu.br>
2024-06-28 18:32:57 -0300

    save 29/06/2024

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,28 +1,26 @@
 module.exports = function (app) {
     app.get("/noticias", function (req, res) {
         var conexao = app.config.dbConnection();
-        conexao.query('select * from noticias', function (error, result) {
+        conexao.query('select *from noticias', function (error, result) {
             res.render('noticias/noticias', { noticias: result })
         });
     })
     app.get("/noticias/:id", function (req, res) {
-        res.send("ola");
+        res.send("ola id");
 
     })
     app.get("/noticias/:Identificacao/:NomeCompleto", function (req, res) {
-        res.send("ola");
+        res.send("ola identificação e nome completo");
 
     })
-    app.post("/noticias/:Identificacao/:NomeCompleto", function (req, res) {
+    app.post("/noticias/:titulo/:noticia", function (req, res) {
         var conexao = app.config.dbConnection();
         var queryNoticias 
-        = "INSERT INTO noticias (titulo,noticia) VALUES ('"
-        +req.params.Identificacao+"','"+req.params.NomeCompleto+"')"; 
-        console.log(queryNoticias);
-        conexao.query(queryNoticias,function (error, result){
-            res.send("olá");    
+        = 'INSERT INTO noticias (tit ulo,noticia) VALUES ("'
+        +req.params.titulo+'","'+req.params.noticia+'")'; 
+        conexao.query(queryNoticias,function (error, result) {
+            console.log(error);
+            res.send(error.errno+" | "+error.sql+" | "+error.sqlMessage);
         })
-        
     })
-
 }
